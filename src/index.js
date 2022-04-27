18e8482d5aa08584fbdd8359c49f0508ae7f813d
Assad Isah <nottherealalanturing@gmail.com>
2022-04-27 07:39:18 +0100

    feat(index.js): add populateDOM() to display tasks on DOM

--- src/index.js
+++ src/index.js
@@ -7,5 +7,18 @@ const tasks = [
 ];
 
 const populateDOM = () => {
-  const tasks = document.querySelector('#tasks');
+  const tasksList = document.querySelector('#tasks');
+  let newList = '';
+  tasks.forEach((val, i) => {
+    newList += `<li class="task">
+    <div class="task-div" data-index=${tasks[i].index}>
+      <input type="checkbox" id="completed" data=${tasks[i].completed}/>
+      <p>${tasks[i].description}</p>
+    </div>
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+  </li>`;
+  });
+  tasksList.innerHTML = newList;
 };
+
+populateDOM();
