0b7676a0a284bc6d43d1bfad19e2ad0e8be9b5d3
Assad Isah <nottherealalanturing@gmail.com>
2022-04-27 06:06:37 +0100

    feat: add some tasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+
+const tasks = [
+  { description: 'Wash the dishes.', completed: false, index: 0 },
+  { description: 'Bake some cake.', completed: false, index: 1 },
+  { description: 'Fix doors.', completed: false, index: 2 },
+];
