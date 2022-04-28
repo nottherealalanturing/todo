60372c1cb5b0b631443ba51552c6a2b7c0a0dc2a
Assad Isah <nottherealalanturing@gmail.com>
2022-04-28 08:04:48 +0100

    feat: create a new class for Tasks

---
+++ src/Tasks.js
@@ -0,0 +1,9 @@
+export default class Tasks {
+  constructor() {
+    this.tasks = [
+      { description: 'Wash the dishes.', completed: false, index: 0 },
+      { description: 'Bake some cake.', completed: false, index: 1 },
+      { description: 'Fix doors.', completed: false, index: 2 },
+    ];
+  }
+}
