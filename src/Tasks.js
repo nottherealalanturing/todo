0b154af7537e3312fa9f82c1a55c3336726ebe4e
Assad Isah <nottherealalanturing@gmail.com>
2022-04-28 08:54:32 +0100

    feat: link addTask function to page

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,18 +1,15 @@
 export default class Tasks {
   static index = 0;
   constructor() {
-    this.tasks = [
-      { description: 'Wash the dishes.', completed: false, index: 0 },
-      { description: 'Bake some cake.', completed: false, index: 1 },
-      { description: 'Fix doors.', completed: false, index: 2 },
-    ];
+    this.tasks = [];
   }
 
   addTask = (description, completed) => {
+    console.log();
     this.tasks.push({
       description: description,
       completed: completed,
-      index: this.index++,
+      index: Tasks.index++,
     });
   };
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import './style.css';
+import Tasks from './Tasks';
+
+const MyTasks = new Tasks();
+
+const addInput = document.querySelector('.newItem');
+
+addInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    MyTasks.addTask(e.target.value, false);
+  }
+});
 
 const populateDOM = () => {
   const tasksList = document.querySelector('#tasks');
   let newList = '';
-  tasks.forEach((val, i) => {
+  MyTasks.tasks.forEach((val, i) => {
     newList += `<li class="task bb">
-    <div class="task-div" data-index=${tasks[i].index}>
-      <input type="checkbox" id="completed" data=${tasks[i].completed}/>
-      <p>${tasks[i].description}</p>
+    <div class="task-div" data-index=${MyTasks.tasks[i].index}>
+      <input type="checkbox" id="completed" data=${MyTasks.tasks[i].completed}/>
+      <p>${MyTasks.tasks[i].description}</p>
     </div>
     <i class="fa-solid fa-ellipsis-vertical"></i>
   </li>`;
