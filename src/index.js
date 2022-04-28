51cf446e4dfeb3711a9c7d50d83f639ffc486cee
Assad Isah <nottherealalanturing@gmail.com>
2022-04-28 08:01:35 +0100

    refactor: remove hardcoded tasklist

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import './style.css';
 
-const tasks = [
-  { description: 'Wash the dishes.', completed: false, index: 0 },
-  { description: 'Bake some cake.', completed: false, index: 1 },
-  { description: 'Fix doors.', completed: false, index: 2 },
-];
-
 const populateDOM = () => {
   const tasksList = document.querySelector('#tasks');
   let newList = '';
